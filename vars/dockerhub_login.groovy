903bf34bac9ab2e3f492774d154529bdb082c71a
M.Ebad Arshad <m.ebadarshad2003@gmail.com>
2025-10-19 01:04:38 +0500

    Fix credentialsId syntax in Dockerhub login script

--- vars/dockerhub_login.groovy
+++ vars/dockerhub_login.groovy
@@ -1,6 +1,6 @@
 def call(String id){
   echo 'This is Dockerhub login stage'
-  withCredentials([usernamePassword(credentialsId: ${id}, usernameVariable: '${USERNAME}', passwordVariable: '${PASSWORD}')]) {
+  withCredentials([usernamePassword(credentialsId: '${id}', usernameVariable: '${USERNAME}', passwordVariable: '${PASSWORD}')]) {
   sh 'docker login -u ${USERNAME} -p ${PASSWORD}'
   }
   echo 'Login to Dockerhub'
