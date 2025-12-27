fb87c051f167120e2da012f47a59ef37c3081004
M.Ebad Arshad <m.ebadarshad2003@gmail.com>
2025-12-27 21:51:19 +0500

    Optimize git clone command with depth option

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,6 +1,6 @@
 def call(String url, String branch){
   echo 'This is clone stage'
-  sh "git clone -b ${branch} ${url} ${branch}"
+  sh "git clone --depth 1 -b ${branch} ${url} ${branch}"
   // git url: "${url}", branch: "${branch}"
   echo 'Cloned successfully'
 }
